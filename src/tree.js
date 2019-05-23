fe05f456b19339fbe01ed4e9bfb7aab7704db9e4
Onisko <Onisko@MacBook.local>
2019-05-23 14:13:59 +0300

    fixed

--- src/tree.js
+++ src/tree.js
@@ -1,33 +1,39 @@
 const data = {
-      "Рыбы": {
-        "Форель": {},
-        "Щука": {}
+      'Рыбы': {
+        'Форель': {},
+        'Щука': {}
       },
 
-      "Деревья": {
-        "Хвойные": {
-          "Лиственница": {},
-          "Ель": {}
+      'Деревья': {
+        'Хвойные': {
+          'Лиственница': {},
+          'Ель': {}
         },
-        "Цветковые": {
-          "Берёза": {},
-          "Тополь": {}
+        'Цветковые': {
+          'Берёза': {},
+          'Тополь': {}
         }
 
       }
 };
 
-let container = document.getElementById('container');
-function createTree(container, obj) {
-  container.innerHTML = createTreeText(obj)
-}
-function createTreeText(obj) {
-  let childLi = '';
-  let parentUl = '';
-  for (let key in obj) {
-    childLi += `<li>${key}${createTreeText(obj[key])}</li>`
-    parentUl = `<ul>${childLi}</ul>`
+function createTreeNode(obj) {
+  const ul = document.createElement('ul');
+  for (const [key, value] of Object.entries(obj)) {
+    const li = document.createElement('li');
+    li.textContent = key;
+    if (Object.keys(value).length) {
+      const subUl = createTreeNode(value);
+      li.appendChild(subUl);
+    }
+    ul.appendChild(li);
   }
-  return parentUl;
+  return ul;
 }
+
+function createTree(container, obj) {
+  container.appendChild(createTreeNode(obj));
+}
+
+let container = document.getElementById('container');
 createTree(container, data);
